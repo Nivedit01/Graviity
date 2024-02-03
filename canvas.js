df1a54985e30c540f93b0581a3f97c9e3686dc32
Radha <Radha_Govind@gmail.com>
2024-02-03 13:38:40 +0530

    Manages code

--- canvas.js
+++ canvas.js
@@ -1,16 +1,20 @@
+// introductory lines to begin with canvas
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+// to control through mouse movements
 const mouse = {
   x: innerWidth / 2,
   y: innerHeight / 2
 }
 
+// colorstack to randomly select colors
 const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
 
+// variables used in the code
 var friction = 0.59;
 
 // utility functions
@@ -21,13 +25,6 @@ function randomIntFromRange(min, max) {
   function randomColor(colors) {
     return colors[Math.floor(Math.random() * colors.length)]
   }
-  
-  function distance(x1, y1, x2, y2) {
-    const xDist = x2 - x1
-    const yDist = y2 - y1
-  
-    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
-  }
 
 // Event Listeners
 addEventListener('click', (event) => {
@@ -36,6 +33,7 @@ addEventListener('click', (event) => {
   init()
 })
 
+// to maintain the effect during resize
 addEventListener('resize', () => {
   canvas.width = innerWidth
   canvas.height = innerHeight
@@ -59,7 +57,7 @@ class Ball {
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
     c.fillStyle = this.color
     c.fill()
-    c.stroke();
+    // c.stroke();
     c.closePath()
   }
 
@@ -103,7 +101,6 @@ function animate() {
   requestAnimationFrame(animate)
   c.clearRect(0, 0, canvas.width, canvas.height)
 
-//   c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
 // Update all balls in the array
     for(var i = 0; i < ballArray.length; i ++){
         ballArray[i].update();
